de7502df427f0b767b52772634c4969c435e71a4
Samara Victoria <Maia_victoria@outlook.com.br>
2024-08-21 22:33:50 -0300

    Aula 05 - Part 1:
     3 Commits em 1
    
     -> Iniciando o boyler plate com a criação do import Cabeçalho;
    
     -> Realizando destruturação com Props;
    
     -> Criando tipos especificos com types;

---
+++ Aula05/src/App.tsx
@@ -0,0 +1,17 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+ 
+export default function App() {
+ 
+  const titulo: string = "Minha página";
+ 
+  const aviso = () => alert(titulo);
+ 
+  return (
+ 
+    <div>
+      <h1>Componente App</h1>
+      <Cabecalho tituloProps={titulo} avisoProps={aviso} />
+    </div>
+ 
+  );
+}
---
+++ Aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,20 @@
+type CabecalhoProps = {
+    tituloProps: string,
+    avisoProps: Function;
+}
+ 
+export default function Cabecalho({ tituloProps, avisoProps }: CabecalhoProps) {
+ 
+    document.title = tituloProps;
+ 
+ 
+ 
+    return (
+        <header>
+            <h1>{tituloProps}</h1>
+            <div>
+                <button onClick={() => avisoProps()}>Aviso do Pai</button>
+            </div>
+        </header>
+    )
+}
