2d6d482a073b7e63e85d3169a23177ef8314f6c8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-21 23:06:52 -0300

    Changed Button Func To Increment + useState

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
 export default function App(){
-  
+
   const title : string = "My Page";
+  const status = "loading";
+
+  const [pageNum, setPageNum] = useState(0);
+
   const myAlert = () => {
-    alert(title);
+    setPageNum(pageNum + 1);
+    console.log("This Function Was Executed: ", title);
   }
 
   return (
     <>
-      <Header title={title} func={myAlert}></Header>
+      <Header title={title} func={myAlert} pageNum={pageNum} status={status}>
+        <h4>Children</h4>
+      </Header>
       <Footer></Footer>
     </>
   );
--- aula05/src/components/Header/index.tsx
+++ aula05/src/components/Header/index.tsx
@@ -1,13 +1,23 @@
 interface Props {
     title: string,
     func: Function,
+    pageNum: number | string,
+    status: "loading" | "deployed",
+    children: React.ReactNode,
 }
 
-export default function Header({ title, func } : Props){
+export default function Header({ title, func, pageNum, status, children } : Props){
+
+    document.title = title + " - " + pageNum
+
     return (
         <header>
-            <h1>{title}</h1>
+            <h1>{title} - {pageNum}</h1>
+            <h2>{status}</h2>
             <button onClick={() => func()}>Click Me!</button>
+            <div>
+                {children}
+            </div>
         </header>
     );
 }
