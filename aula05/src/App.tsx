7273f0f10251b7934fd834519cd272c6fb8d46fb
Dametto98 <caikedametto98@gmail.com>
2024-08-21 21:49:30 -0300

    iniciando o boyler plate com a criação e import do cabecalho.

---
+++ aula05/src/App.tsx
@@ -0,0 +1,10 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
+export default function App(){
+  return(
+    <div>
+      <h1>Componente App</h1>
+      <Cabecalho />
+    </div>
+  )
+}
