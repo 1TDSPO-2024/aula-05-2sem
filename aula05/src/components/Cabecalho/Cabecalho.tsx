d4224ea75c0a95876a96921d1a2655105440ed53
GuiJanunzzi <guijanunzzi7@gmail.com>
2024-08-21 23:01:38 -0300

    Props children

--- aula05/src/components/Cabecalho/Cabecalho.tsx
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -1,11 +1,14 @@
-type CabecalhoProps = {
+type Cabecalho1Props = {
     paginaProps: string; 
     nrpaginaProps: number | string
+    children:React.ReactNode
+}
+type Cabecalho2Props = {
     statusProps: "loading" | "deployed" | "legal"
     avisoProps: Function;
 }
 
-export default function Cabecalho({paginaProps, nrpaginaProps, statusProps, avisoProps}: CabecalhoProps){
+export default function Cabecalho({paginaProps, nrpaginaProps, statusProps, avisoProps, children }: Cabecalho1Props & Cabecalho2Props){
 
     document.title = statusProps+ " - " +nrpaginaProps;
 
@@ -14,6 +17,9 @@ export default function Cabecalho({paginaProps, nrpaginaProps, statusProps, avis
             <h1>{paginaProps + " - " +nrpaginaProps}</h1>
             <div>
                 <button onClick={()=> avisoProps()}>Aviso do Pai</button>
+            </div>
+            <div>
+                {children}
             </div>
         </header>
     );
