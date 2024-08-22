0d4e093a240fa456cb92585b0a3921d0f6e0c610
FrancescoDiBenedetto <francescomdibe@gmail.com>
2024-08-21 22:22:28 -0300

    desetruturando props

--- aula05/src/components/Cabecalho/Cabecalho.tsx
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -1,12 +1,26 @@
-export default function Cabecalho(props:{tituloProps:string, avisoProps: Function}) {
+export default function Cabecalho({tituloProps, avisoProps}:{tituloProps:string, avisoProps: Function}) {
   
-    document.title = props.tituloProps
+    document.title = tituloProps
+
+    const aluno = {
+        nome: "nuno",
+        idade: 11,
+        rm: 346789
+    }
+
+    // const{nome, idade, rm} = aluno
+    const{nome, ...resto} = aluno
 
     return (
     <header>
-      <h1>{props.tituloProps}</h1>
+      <h1>{tituloProps}</h1>
       <div>
-        <button onClick={()=> props.avisoProps()}>Aviso do pai</button>
+        <button onClick={()=> avisoProps()}>Aviso do pai</button>
+        <div>
+            <p>Nome: {nome}</p>
+            <p>idade: {resto.idade}</p>
+            <p>rm: {resto.rm}</p>
+        </div>
       </div>
     </header>
   );
