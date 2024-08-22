4aae9745f8f34c89edf14f5a23f19ee7e3943e9f
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-21 22:46:54 -0300

    Union-types e Intersection com valores.

--- aula05/src/components/Cabecalho/Cabecalho.tsx
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -1,15 +1,17 @@
 type CabecalhoProps = {
-    tituloProps: string;
+    paginaProps: string;
+    nrPaginaProps: number | string;
+    statusProps: "loading" | "deployed" | "xuxu";
     avisoProps: Function;
 }
 
-export default function Cabecalho({ tituloProps, avisoProps }: CabecalhoProps) {
+export default function Cabecalho({ paginaProps, nrPaginaProps, statusProps, avisoProps }: CabecalhoProps) {
 
-    document.title = tituloProps;
+    document.title = statusProps + " - " + nrPaginaProps;
 
     return (
         <header>
-            <h1>{tituloProps}</h1>
+            <h1>{paginaProps + " - " + nrPaginaProps}</h1>
             <div>
                 <button onClick={() => avisoProps()}>Aviso do Pai</button>
             </div>
