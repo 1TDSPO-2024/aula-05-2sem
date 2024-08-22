ebb3f6c2ae784d71a9b0105da3727f46e50dbdbd
fesilva2109 <felipesilvaa.2109@gmail.com>
2024-08-21 23:07:02 -0300

    Convertendo type em interface e exportando

---
+++ aula05/src/types.ts
@@ -0,0 +1,9 @@
+export type Cabecalho1Props = {
+    paginaProps: string,
+    nrPaginaProps: number | string
+    children: React.ReactNode
+}
+export type Cabecalho2Props = {
+    statusProps: "loading" | "deployed"
+    avisoProps: Function
+}
