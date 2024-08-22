5a75988fa648eef47a01914071bd43f408e52858
GuiJanunzzi <guijanunzzi7@gmail.com>
2024-08-21 22:22:05 -0300

    Realizando desestruturação(destructuring) com PROPS... :p

--- aula05/src/components/Cabecalho/Cabecalho.tsx
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -1,14 +1,26 @@
-export default function Cabecalho(props:{tituloProps: string, avisoProps: Function }){
+export default function Cabecalho({tituloProps, avisoProps}:{tituloProps: string, avisoProps: Function }){
 
-    document.title = props.tituloProps;
+    document.title = tituloProps;
 
+    const aluno = {
+        nome: "Marquinhos",
+        idade: 11,
+        rm: 556789
+    }
 
+    // const{nome, idade, rm} = aluno;
+    const {nome, ...resto} = aluno;
 
     return(
         <header>
-            <h1>{props.tituloProps}</h1>
+            <h1>{tituloProps}</h1>
             <div>
-                <button onClick={()=> props.avisoProps()}>Aviso do Pai</button>
+                <button onClick={()=> avisoProps()}>Aviso do Pai</button>
+            </div>
+            <div>
+                <p>Nome: {nome}</p>
+                <p>Idade: {resto.idade}</p>
+                <p>RM: {resto.rm}</p>
             </div>
         </header>
     );
