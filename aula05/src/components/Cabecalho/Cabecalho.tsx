e0e890adf4be05b46a2191f3f5c7e1e1018540d7
GMGrizao <guilhermemenezes2102@gmail.com>
2024-08-21 21:49:34 -0300

    Iniciando o boyler plate com a criação e import do cabeçalho.

---
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,10 @@
+export default function Cabecalho() {
+    return (
+        <div>
+            <header>
+                <h1>Component App</h1>
+                <Cabecalho />
+            </header>
+        </div>
+    )
+}
