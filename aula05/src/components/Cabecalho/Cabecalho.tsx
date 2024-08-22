969319e3b74ce807bc103ba1034e89c9171aca94
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-21 22:02:06 -0300

    Passando props por variável.

--- aula05/src/components/Cabecalho/Cabecalho.tsx
+++ aula05/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,10 @@
-export default function Cabecalho() {
+export default function Cabecalho(props: { tituloProps: string }) {
+
+    document.title = props.tituloProps;
+
     return (
         <header>
-            <h1>Cabeçalho</h1>
+            <h1>{props.tituloProps}</h1>
         </header>
     );
 }
