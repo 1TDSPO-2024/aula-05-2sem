4be081e47c8bc654fdfb6518c296a87d206e915a
tcapela <trc2405@gmail.com>
2024-08-21 22:26:45 -0300

    Criando tipos especificos com types

--- aula05/src/components/cabecalho/Cabecalho.tsx
+++ aula05/src/components/cabecalho/Cabecalho.tsx
@@ -1,25 +1,17 @@
-export default function Cabecalho({tituloProps, avisoProps}: { tituloProps: string, avisoProps: Function}) {
+type CabecalhoProps = {
+    tituloProps: string;
+    avisoProps: Function;
+}
+
+export default function Cabecalho({tituloProps, avisoProps}: CabecalhoProps) {
 
     document.title = tituloProps;
-
-    const aluno = {
-        nome: "Rayssa",
-        idade: 18,
-        rm: 558021
-    }
-
-    const{nome, idade, rm} = aluno;
-
+       
     return(
         <header>
             <h1>{tituloProps}</h1>
             <div>
                 <button onClick={() => avisoProps()}>Aviso do Pai</button>
-                <div>
-                    <p>Nome: {nome}</p>
-                    <p>Idade: {idade}</p>
-                    <p>RM: {rm}</p>
-                </div>
             </div>
         </header>
     )
